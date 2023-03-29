cad8ce0bf9cc686655a4e1a0a8eb071366aca2a0
betterclient <iamanaltlolwhypog@gmail.com>
2023-03-29 22:10:13 +0300

    b1.7.3

--- src/main/java/com/example/template/mixin/MixinExample.java
+++ src/main/java/com/example/template/mixin/MixinExample.java
@@ -1,8 +1,8 @@
 package com.example.template.mixin;
 
-import net.minecraft.client.GuiScreen;
-import net.minecraft.client.Session;
 import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.Session;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,13 +10,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = GuiMainMenu.class)
 public class MixinExample extends GuiScreen {
-
-    @Inject(method = "initGui", at = @At("HEAD"))
-    public void initGui1(CallbackInfo ci) { //Show Quit Game button
-        this.mc.field_6317_l = false;
-    }
-
-
     @Inject(method = "initGui", at = @At("RETURN"))
     public void initGui(CallbackInfo ci) {
         this.mc.session = new Session("Player", "");
